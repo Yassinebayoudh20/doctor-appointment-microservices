d4103e306056183a2f2fb2e236b4612f53ee7fd6
YassineBayoudh <bayoudh.yassine20@gmail.com>
2021-10-16 12:13:53 +0100

    add user-service + jwt access

---
+++ user-auth-service/src/test/java/edu/esprit/projetmicroservice/userauthservice/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.esprit.projetmicroservice.userauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
